09e961da485976bddbc8f3e31aadb1bd7f48db48
Tom Pan <tom_pan@seu.edu.cn>
2023-05-28 20:30:33 +0800

    Add a post
    init

---
+++ blog/.vuepress/config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  title: 'Tom\'s Blog',
+  description: '记录技术思考',
+  theme: '@vuepress/theme-blog', // OR shortcut: @vuepress/blog
+  base: '/blog/',
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#modifyblogpluginoptions
+     */
+    modifyBlogPluginOptions(blogPluginOptions) {
+      return blogPluginOptions
+    },
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/tompan-1901',
+        },
+        // {
+        //   type: 'twitter',
+        //   link: 'https://twitter.com/_ulivz',
+        // },
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2018-present Vue.js',
+          link: '',
+        },
+      ],
+    },
+  },
+}
